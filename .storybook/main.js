cf7eeda2e40ef278c6190e1310b01ed6ec38ac01
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-12-05 12:51:36 +0200

    Add storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,29 +1,139 @@
+const webpack = require('webpack');
 const path = require('path');
-const lessPlugin = require('./webpack-less-plugin');
+const makeLoaderFinder = require('razzle-dev-utils/makeLoaderFinder');
+const fileLoaderFinder = makeLoaderFinder('file-loader');
+
+const projectRootPath = path.resolve('.');
+const createAddonsLoader = require('@plone/volto/create-addons-loader');
+const lessPlugin = require('@plone/volto/webpack-less-plugin');
+
+const createConfig = require('../node_modules/razzle/config/createConfigAsync.js');
+const razzleConfig = require(path.join(projectRootPath, 'razzle.config.js'));
+
+const SVGLOADER = {
+  test: /icons\/.*\.svg$/,
+  use: [
+    {
+      loader: 'svg-loader',
+    },
+    {
+      loader: 'svgo-loader',
+      options: {
+        plugins: [
+          { removeTitle: true },
+          { convertPathData: false },
+          { removeUselessStrokeAndFill: true },
+          { removeViewBox: false },
+        ],
+      },
+    },
+  ],
+};
 
 module.exports = {
-  stories: [
-    '../node_modules/@plone/volto/src/**/*.stories.mdx',
-    '../node_modules/@plone/volto/src/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/addons/**/**/*.stories.mdx',
-    '../src/addons/**/**/*.stories.@(js|jsx|ts|tsx)',
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    // '@storybook/preset-scss',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async (config) => {
-    config.resolve.alias['@eeacms/eea-design-system'] = path.join(
-      __dirname,
-      './../src',
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    let baseConfig;
+    baseConfig = await createConfig(
+      'web',
+      'dev',
+      {
+        // clearConsole: false,
+        modifyWebpackConfig: razzleConfig.modifyWebpackConfig,
+        plugins: razzleConfig.plugins,
+      },
+      webpack,
     );
-    config.resolve.alias['../../theme.config'] = path.resolve(
-      __dirname,
-      '../theme/theme.config',
-    );
-    config.resolve.alias['../../theme.config$'] = path.resolve(
-      __dirname,
-      '../theme/theme.config',
+    const AddonConfigurationRegistry = require('@plone/volto/addon-registry');
+
+    const registry = new AddonConfigurationRegistry(projectRootPath);
+
+    config = lessPlugin({ registry }).modifyWebpackConfig({
+      env: { target: 'web', dev: 'dev' },
+      webpackConfig: config,
+      webpackObject: webpack,
+      options: {},
+    });
+
+    // putting SVG loader on top, fix the fileloader manually (Volto plugin does not
+    // work) since it needs to go first
+    config.module.rules.unshift(SVGLOADER);
+    const fileLoader = config.module.rules.find(fileLoaderFinder);
+    fileLoader.exclude = [/\.(config|variables|overrides)$/, /icons\/.*\.svg$/];
+
+    config.plugins.unshift(
+      new webpack.DefinePlugin({
+        __DEVELOPMENT__: true,
+        __CLIENT__: true,
+        __SERVER__: false,
+      }),
     );
 
-    lessPlugin(config);
-    return config;
+    const resultConfig = {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: { ...config.resolve.alias, ...baseConfig.resolve.alias },
+      },
+    };
+    resultConfig.module.rules[1].exclude = /node_modules\/(?!(@plone\/volto)\/)/;
+    // console.dir(resultConfig.module.rules[1].exclude, { depth: null });
+
+    return resultConfig;
+  },
+  babel: async (options) => {
+    return {
+      ...options,
+      plugins: [
+        ...options.plugins,
+        [
+          './node_modules/babel-plugin-root-import/build/index.js',
+          {
+            rootPathSuffix: './src',
+          },
+        ],
+      ],
+      // any extra options you want to set
+    };
   },
 };
+
+// const path = require('path');
+// const lessPlugin = require('./webpack-less-plugin');
+//
+// module.exports = {
+//   stories: [
+//     '../node_modules/@plone/volto/src/**/*.stories.mdx',
+//     '../node_modules/@plone/volto/src/**/*.stories.@(js|jsx|ts|tsx)',
+//     '../src/addons/**/**/*.stories.mdx',
+//     '../src/addons/**/**/*.stories.@(js|jsx|ts|tsx)',
+//   ],
+//   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+//   webpackFinal: async (config) => {
+//     config.resolve.alias['@eeacms/eea-design-system'] = path.join(
+//       __dirname,
+//       './../src',
+//     );
+//     config.resolve.alias['../../theme.config'] = path.resolve(
+//       __dirname,
+//       '../theme/theme.config',
+//     );
+//     config.resolve.alias['../../theme.config$'] = path.resolve(
+//       __dirname,
+//       '../theme/theme.config',
+//     );
+//
+//     lessPlugin(config);
+//     return config;
+//   },
+// };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
+// import '@plone/volto/config'; // This is the bootstrap for the global config - client side
+
+import '~/config'; // This is the bootstrap for the global config - client side
+import React from 'react';
+import { StaticRouter } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
+import enMessages from '~/../locales/en.json';
+
+import '~/theme';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,3 +17,14 @@ export const parameters = {
     },
   },
 }
+
+
+export const decorators = [
+  (Story) => (
+    <IntlProvider messages={enMessages} locale="en" defaultLocale="en">
+      <StaticRouter location="/">
+        <Story />
+      </StaticRouter>
+    </IntlProvider>
+  ),
+];
