42d7c099b489caef72564070ff661555e5027735
ITML <stamatis@itml.gr>
2021-12-23 14:11:37 +0000

    Added storybook timeline. Added assets folder. Added storybook theme.

---
+++ .storybook/eea-theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+import logo from '@eeacms/volto-eea-design-system/theme/themes/eea/assets/images/EEA_Logo_Vertical.png';
+
+export default create({
+  base: 'light',
+  brandTitle: 'EEA Component Library',
+  brandUrl: 'https://www.eea.europa.eu/',
+  brandImage: logo,
+});
