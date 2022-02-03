bbf202d23dd49268460f93b6e86ebe4496d343ff
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-02-03 16:30:01 +0200

    Add plugin

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,14 @@
 // import '@plone/volto/config'; // This is the bootstrap for the global config - client side
 
 import '~/config'; // This is the bootstrap for the global config - client side
+
+import '@eeacms/volto-eea-design-system/semantic.less';
+import '@plone/volto/../theme/themes/pastanaga/extras/extras.less';
+
 import React from 'react';
 import { StaticRouter } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
 import enMessages from '~/../locales/en.json';
-
-import '@eeacms/volto-eea-design-system/semantic.less';
-import '@plone/volto/../theme/themes/pastanaga/extras/extras.less';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
