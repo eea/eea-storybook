dd072ccb3a9e11a64e0fed095806baf7da2f4653
David Ichim <ichim.david@gmail.com>
2022-02-18 21:01:09 +0200

    feat(storybook): added several mobile and desktop resolutions to the responsive preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,15 +8,63 @@ import enMessages from '~/../locales/en.json';
 
 import '@eeacms/volto-eea-design-system/semantic.less';
 import '@plone/volto/../theme/themes/pastanaga/extras/extras.less';
+import {
+  MINIMAL_VIEWPORTS
+} from '@storybook/addon-viewport';
+
+const customMobileViewports = {
+  galaxys9: {
+    name: 'Galaxy S9',
+    styles: {
+      height: '740px',
+      width: '360px',
+    },
+    type: 'mobile',
+  },
+};
+
+const customDesktopViewports = {
+  tablet: {
+    name: '1024x768',
+    styles: {
+      height: '768px',
+      width: '1024px',
+    },
+    type: 'desktop',
+  },
+  laptop: {
+    name: '1280x800',
+    styles: {
+      height: '800px',
+      width: '1280px',
+    },
+    type: 'desktop',
+  },
+  widescreen: {
+    name: '1366x768',
+    styles: {
+      height: '768px',
+      width: '1366px',
+    },
+    type: 'desktop',
+  },
+};
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: {
+      ...customMobileViewports,
+      ...MINIMAL_VIEWPORTS,
+      ...customDesktopViewports,
+    },
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
-    expanded:false,
+    expanded: false,
   },
 };
 
