fd8a98c3a63553b283d5f6004a664d67cb36f150
David Ichim <ichim.david@gmail.com>
2022-07-31 19:10:40 +0300

    change(tests): added fixes from volto-eea-design-system to volto-kitkat-frontend

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -27,7 +27,7 @@ coverage-end */
 export const setupBeforeEach = () => {
   cy.autologin();
   cy.createContent({
-    contentType: 'Folder',
+    contentType: 'Document',
     contentId: 'cypress',
     contentTitle: 'Cypress',
   });
@@ -38,13 +38,12 @@ export const setupBeforeEach = () => {
     path: 'cypress',
   });
   cy.visit('/cypress/my-page');
-  cy.waitForResourceToLoad('@navigation');
   cy.waitForResourceToLoad('@breadcrumbs');
   cy.waitForResourceToLoad('@actions');
   cy.waitForResourceToLoad('@types');
   cy.waitForResourceToLoad('my-page');
   cy.navigate('/cypress/my-page/edit');
-  cy.get(`.block.title [data-contents]`);
+  cy.get(`.block.title [data-slate-editor]`);
 };
 
 export const tearDownAfterEach = () => {
