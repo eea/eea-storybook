f5d97839a4b62dedffe0b533804c3c60f0e96673
David Ichim <ichim.david@gmail.com>
2022-11-04 20:08:52 +0200

    change(storybook): downgrade to 6.4.22
    - until we check 6.5.13 if our stories are compatible

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,59 +9,107 @@ const createConfig = require('../node_modules/razzle/config/createConfigAsync.js
 const razzleConfig = require(path.join(projectRootPath, 'razzle.config.js'));
 const SVGLOADER = {
   test: /icons\/.*\.svg$/,
-  use: [{
-    loader: 'svg-loader'
-  }, {
-    loader: 'svgo-loader',
-    options: {
-      plugins: [{
-        removeTitle: true
-      }, {
-        convertPathData: false
-      }, {
-        removeUselessStrokeAndFill: true
-      }, {
-        removeViewBox: false
-      }]
-    }
-  }]
+  use: [
+    {
+      loader: 'svg-loader',
+    },
+    {
+      loader: 'svgo-loader',
+      options: {
+        plugins: [
+          {
+            removeTitle: true,
+          },
+          {
+            convertPathData: false,
+          },
+          {
+            removeUselessStrokeAndFill: true,
+          },
+          {
+            removeViewBox: false,
+          },
+        ],
+      },
+    },
+  ],
 };
+
+const defaultRazzleOptions = {
+  verbose: false,
+  debug: {},
+  buildType: 'iso',
+  cssPrefix: 'static/css',
+  jsPrefix: 'static/js',
+  enableSourceMaps: true,
+  enableReactRefresh: true,
+  enableTargetBabelrc: false,
+  enableBabelCache: true,
+  forceRuntimeEnvVars: [],
+  mediaPrefix: 'static/media',
+  staticCssInDev: false,
+  emitOnErrors: false,
+  disableWebpackbar: false,
+  browserslist: [
+    '>1%',
+    'last 4 versions',
+    'Firefox ESR',
+    'not ie 11',
+    'not dead',
+  ],
+};
+
 module.exports = {
-  stories: ['../src/addons/volto-eea-design-system/src/ui/**/*.stories.mdx', '../src/addons/volto-eea-design-system/src/ui/**/*.stories.@(js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', '@whitespace/storybook-addon-html'],
+  stories: [
+    '../src/addons/volto-eea-design-system/src/ui/**/*.stories.mdx',
+    '../src/addons/volto-eea-design-system/src/ui/**/*.stories.@(js|jsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@whitespace/storybook-addon-html',
+  ],
   features: {
-    storyStoreV7: true,
-    buildStoriesJson: true,
+    // storyStoreV7: true,
+    // buildStoriesJson: true,
     emotionAlias: false,
     postcss: false,
-    modernInlineRender: true
+    // modernInlineRender: true,
   },
-  webpackFinal: async (config, {
-    configType
-  }) => {
+  webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
 
     // Make whatever fine-grained changes you need
     let baseConfig;
-    baseConfig = await createConfig('web', 'dev', {
-      // clearConsole: false,
-      modifyWebpackConfig: razzleConfig.modifyWebpackConfig,
-      plugins: razzleConfig.plugins
-    }, webpack);
+    baseConfig = await createConfig(
+      'web',
+      'dev',
+      {
+        // clearConsole: false,
+        modifyWebpackConfig: razzleConfig.modifyWebpackConfig,
+        plugins: razzleConfig.plugins,
+      },
+      webpack,
+      false,
+      undefined,
+      [],
+      defaultRazzleOptions,
+    );
     const AddonConfigurationRegistry = require('@plone/volto/addon-registry');
     const registry = new AddonConfigurationRegistry(projectRootPath);
     config = lessPlugin({
-      registry
+      registry,
     }).modifyWebpackConfig({
       env: {
         target: 'web',
-        dev: true
+        dev: true,
       },
       webpackConfig: config,
       webpackObject: webpack,
-      options: {}
+      options: {},
     });
 
     // putting SVG loader on top, fix the fileloader manually (Volto plugin does not
@@ -69,18 +117,24 @@ module.exports = {
     config.module.rules.unshift(SVGLOADER);
     const fileLoader = config.module.rules.find(fileLoaderFinder);
     fileLoader.exclude = [/\.(config|variables|overrides)$/, /icons\/.*\.svg$/];
-    config.plugins.unshift(new webpack.DefinePlugin({
-      __DEVELOPMENT__: true,
-      __CLIENT__: true,
-      __SERVER__: false
-    }));
+    config.plugins.unshift(
+      new webpack.DefinePlugin({
+        __DEVELOPMENT__: true,
+        __CLIENT__: true,
+        __SERVER__: false,
+      }),
+    );
     const razzleOptions = {
-      cssPrefix: 'css'
+      cssPrefix: 'css',
     };
     const experimental = {};
     const miniPlugin = new MiniCssExtractPlugin({
-      filename: `${razzleOptions.cssPrefix}/bundle.[${experimental.newContentHash ? 'contenthash' : 'chunkhash'}:8].css`,
-      chunkFilename: `${razzleOptions.cssPrefix}/[name].[${experimental.newContentHash ? 'contenthash' : 'chunkhash'}:8].chunk.css`
+      filename: `${razzleOptions.cssPrefix}/bundle.[${
+        experimental.newContentHash ? 'contenthash' : 'chunkhash'
+      }:8].css`,
+      chunkFilename: `${razzleOptions.cssPrefix}/[name].[${
+        experimental.newContentHash ? 'contenthash' : 'chunkhash'
+      }:8].chunk.css`,
     });
     config.plugins.unshift(miniPlugin);
     const resultConfig = {
@@ -89,26 +143,40 @@ module.exports = {
         ...config.resolve,
         alias: {
           ...config.resolve.alias,
-          ...baseConfig.resolve.alias
-        }
-      }
+          ...baseConfig.resolve.alias,
+        },
+      },
     };
     resultConfig.module.rules[1].exclude = /node_modules\/(?!(@plone\/volto)\/)/;
-    const addonExtenders = registry.getAddonExtenders().map(m => require(m));
-    const extendedConfig = addonExtenders.reduce((acc, extender) => extender.modify(acc, {
-      target: 'web',
-      dev: 'dev'
-    }, config), resultConfig);
+    const addonExtenders = registry.getAddonExtenders().map((m) => require(m));
+    const extendedConfig = addonExtenders.reduce(
+      (acc, extender) =>
+        extender.modify(
+          acc,
+          {
+            target: 'web',
+            dev: 'dev',
+          },
+          config,
+        ),
+      resultConfig,
+    );
     return extendedConfig;
   },
-  babel: async options => {
+  babel: async (options) => {
     return {
       ...options,
-      plugins: [...options.plugins, ['./node_modules/babel-plugin-root-import/build/index.js', {
-        rootPathSuffix: './src'
-      }]]
+      plugins: [
+        ...options.plugins,
+        [
+          './node_modules/babel-plugin-root-import/build/index.js',
+          {
+            rootPathSuffix: './src',
+          },
+        ],
+      ],
       // any extra options you want to set
     };
   },
-  framework: '@storybook/html'
+  framework: '@storybook/html',
 };
