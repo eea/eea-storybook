31721dd32d8e8e9593ac1341069b819c15790f9f
David Ichim <ichim.david@gmail.com>
2025-07-25 14:41:46 +0300

    fix(stories): bring back specific addons reference to avoid OOM crash
    - for some reason with addons/** the storybook build process was crashing

--- .storybook/main.js
+++ .storybook/main.js
@@ -61,10 +61,7 @@ const defaultRazzleOptions = {
 
 module.exports = {
   // reactOptions: { legacyRootApi: true },
-  stories: [
-    '../src/addons/**/*.stories.mdx',
-    '../src/addons/**/*.stories.@(js|jsx)',
-  ],
+  stories: ['../src/addons/volto-eea-design-system/src/ui/**/*.stories.mdx', '../src/addons/volto-eea-design-system/src/ui/**/*.stories.@(js|jsx)', '../src/addons/volto-plotlycharts/src/**/*.stories.jsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', '@whitespace/storybook-addon-html'],
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
