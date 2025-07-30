91251a6a92f33a684b3352bbdcf8f983a649096c
Teodor <teodorvoicu87@gmail.com>
2025-07-30 13:24:01 +0300

    fix path for version info component

--- .storybook/main.js
+++ .storybook/main.js
@@ -61,7 +61,7 @@ const defaultRazzleOptions = {
 
 module.exports = {
   // reactOptions: { legacyRootApi: true },
-  stories: ['../src/addons/volto-eea-design-system/src/ui/**/*.stories.mdx', '../src/addons/volto-eea-design-system/src/ui/**/*.stories.@(js|jsx)', '../src/addons/volto-plotlycharts/src/**/*.stories.jsx'],
+  stories: ['../src/components/**/*.stories.jsx', '../src/addons/volto-eea-design-system/src/ui/**/*.stories.mdx', '../src/addons/volto-eea-design-system/src/ui/**/*.stories.@(js|jsx)', '../src/addons/volto-plotlycharts/src/**/*.stories.jsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', '@whitespace/storybook-addon-html'],
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
